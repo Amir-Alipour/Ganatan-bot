81ca499297d9980d40e603426f0455b0e96ece80
Amir Alipour <amir.alipour12127@gmail.com>
2021-07-18 16:36:22 +0430

    added cmd command

--- src/Bot.js
+++ src/Bot.js
@@ -25,5 +25,30 @@ client.on("message", async (message) => {
         message.reply(`What you need boss ${"https://emoji.gg/assets/emoji/6308-cow-roll.gif"}`);
     }
 
-    
+    if(message.content.startsWith(cmd)){
+        const [CMD_NAME, ...args] = message.content
+            .trim()
+            .substring(cmd.length)
+            .split(/\s+/);
+
+        switch (CMD_NAME){
+            case "cmd": {
+                message.reply(`Ganatan Commands :
+                    ${'`$p (song name | artist and song)`'} : play song
+                    ${'`$s or $stop`'} : pause the song
+                    ${'`$r`'} : resume the song
+                    ${'`$mute and $unmute`'} : mute or unmute the Ganatan
+                    ${'`$vol (up | down)`'} : increase or decrease volume
+                    ${'`$dis`'} : disconnect Ganatan
+                `);
+                break;
+            }
+
+            
+
+            default: {
+                message.reply("I don't know this command");
+            }
+        }
+    }
 })
