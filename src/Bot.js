859cdab50ceb0c770bcf5d2aedd7cc8854f8109a
Amir Alipour <amir.alipour12127@gmail.com>
2021-07-18 16:21:41 +0430

    BOT base is ready

--- src/Bot.js
+++ src/Bot.js
@@ -1,1 +1,29 @@
+// env file for hidden keys
 require('dotenv').config();
+
+// axios for manage HTTP requests
+const axios = require('axios').default;
+
+// for get video IDs from a query at youtube
+const usetube = require("usetube");
+
+//  for better connection to discord Api with JS
+const {Client} = require('discord.js');
+const client = new Client();
+client.login(process.env.TOKEN);
+
+const cmd = "$" // starter command for know when user call BOT
+
+client.on("ready", () => {
+    console.log("BOT is ready!");
+})
+
+client.on("message", async (message) => {
+    if(message.author.bot) return;
+
+    if(message.content.toLocaleLowerCase() === "ganatan"){
+        message.reply(`What you need boss ${"https://emoji.gg/assets/emoji/6308-cow-roll.gif"}`);
+    }
+
+    
+})
