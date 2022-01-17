e0b0b02c41b4578552ac9c494c721cc2c4a6d300
amir alipour <amir.alipour12127@gmail.com>
2022-01-17 16:20:39 +0330

    update permissions

--- src/Bot.js
+++ src/Bot.js
@@ -11,21 +11,10 @@ const usetube = require("usetube");
 const { Client, MessageEmbed, MessageButton, MessageActionRow, Intents } = require("discord.js");
 const client = new Client({ 
 	intents: [
-        Intents.FLAGS,
 		Intents.FLAGS.GUILDS, 
 		Intents.FLAGS.GUILD_MESSAGES,
         Intents.FLAGS.GUILD_VOICE_STATES,
-        Intents.FLAGS.GUILD_WEBHOOKS,
-        Intents.FLAGS.GUILD_SCHEDULED_EVENTS,
-        Intents.FLAGS.GUILD_PRESENCES,
         Intents.FLAGS.GUILD_MESSAGE_TYPING,
-        Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
-        Intents.FLAGS.GUILD_MESSAGES,
-        Intents.FLAGS.GUILD_MEMBERS,
-        Intents.FLAGS.GUILD_INVITES,
-        Intents.FLAGS.GUILD_INTEGRATIONS,
-        Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
-        Intents.FLAGS.GUILD_BANS
 	] 
 });
 client.login(process.env.TOKEN);
